9826c1bbcd3ee78cf41281a529799712fb48e101
manami young <manayou1125@outlook.jp>
2022-08-13 19:29:26 +0900

    Inplement a new action of the task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,8 +8,16 @@ class TasksController < ApplicationController
 		@task = Task.find(params[:id])
 	end 
 
-	private
+	def new 
+		@task = Task.new 
+	end 
+
+	def edit 
+		@task = Task.find(params[:id]) 
+	end 
 	
+	private
+
 	def task_params 
 		params.require(:task).permit(:title, :content)
 	end 
