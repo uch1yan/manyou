bd57795b8a68fc73ad47e1e7b50ff015f408ade7
manami young <manayou1125@outlook.jp>
2022-08-13 20:00:59 +0900

    add a flash function to create and update actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,10 +12,30 @@ class TasksController < ApplicationController
 		@task = Task.new 
 	end 
 
+	def create 
+		@task = Task.new(task_params)
+		if @task.save 
+			redirect_to tasks_path
+			flash[:success] = 'Taskを登録しました!'
+		else 
+			render :new 
+		end 
+	end 
+
 	def edit 
 		@task = Task.find(params[:id]) 
 	end 
-	
+
+	def update 
+		@task = Task.find(params[:id])
+		if @task.update(task_params)
+			redirect_to tasks_path 
+			flash[:success] = 'Taskを更新しました!'
+		else  
+			render :edit 
+		end 
+	end  
+
 	private
 
 	def task_params 
