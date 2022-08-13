ed9c77b79e9df978452efb68a75e7fb7ca17e62e
manami young <manayou1125@outlook.jp>
2022-08-13 19:12:17 +0900

    Implement index and show action of the task controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,17 @@
+class TasksController < ApplicationController
+
+	def index 
+		@tasks = Task.all  
+	end 
+
+	def show 
+		@task = Task.find(params[:id])
+	end 
+
+	private
+	
+	def task_params 
+		params.require(:task).permit(:title, :content)
+	end 
+end
+
