b31ccc6291b93360ebaaaa2cc82779dad1f5284f
manami young <manayou1125@outlook.jp>
2022-08-24 12:04:28 +0900

    add an admin and fix index and new pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :tasks 
-  resources :users, only: [:new, :create, :show, :destroy]
+  resources :users, only: [:index, :new, :create, :show, :destroy]
+
+  namespace :admin do 
+    resources :users 
+  end 
 end
