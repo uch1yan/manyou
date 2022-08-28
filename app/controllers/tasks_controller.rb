3187bcfb53e3f424564064ac4b1e0e9e772b91e3
manami young <manayou1125@outlook.jp>
2022-08-28 10:18:56 +0900

    heroku deproy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,7 +35,7 @@ class TasksController < ApplicationController
   end
 
 	def create 
-		@task = Task.new(task_params)
+		 @task = current_user.tasks.build(task_params)
 		if @task.save
 			redirect_to tasks_path
 			flash[:success] = 'Taskを登録しました!'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
+  root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :tasks 
   resources :users
