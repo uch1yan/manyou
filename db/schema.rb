7006c5c765d9415deb23da21e1f65648b0a75157
manami young <manayou1125@outlook.jp>
2022-08-28 12:07:17 +0900

    deploy

--- db/schema.rb
+++ db/schema.rb
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 2022_08_24_044209) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "email"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
