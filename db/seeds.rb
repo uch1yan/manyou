c5f03f558bd121357890bc16dd0cf461a529f2f2
manami young <manayou1125@outlook.jp>
2022-08-30 16:02:27 +0900

    fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,50 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: "admin", email: "admin@gmail.com", password: "000000", password_confirmation: "000000", admin: false)
-User.create!(name: "manami", email: "manami@gmail.com", password: "000000", password_confirmation: "000000", admin: false)
-User.create!(name: "bay", email: "bay@gmail.com", password: "000000", password_confirmation: "000000", admin: true)
+User.create!(name: "test_admin", email: "test_admin@gmail.com", password: "000000", password_confirmation: "000000", admin: true)
 
 10.times do |n|
   User.create!(
     email: "test#{n + 1}@test.com",
     name: "test_User#{n + 1}",
-    password: "000000"
+    password: "000000",
     password_confirmation: "000000",
     admin: false
   )
 end
 
-10.times do |n|
+3.times do |n|
   Task.create!(
-    name: "Task#{n + 1}",
+    title: "Task#{n + 1}",
+    content: "task_content#{n + 1}",
+    deadline: "2022-09-01",
+    status: "未着手",
+    priority: "高"
+  )
+end
+
+3.times do |n|
+  Task.create!(
+    title: "Task#{n + 4}",
+    content: "task_content#{n + 1}",
+    deadline: "2022-09-15",
+    status: "着手中",
+    priority: "中"
+  )
+end
+
+3.times do |n|
+  Task.create!(
+    title: "Task#{n + 7}",
+    content: "task_content#{n + 1}",
+    deadline: "2022-09-30",
+    status: "完了",
+    priority: "低"
   )
 end
 
 10.times do |n|
   Label.create!(
-    name: "Label#{n + 1}",
+    name: "Label#{n + 1}"
   )
 end
