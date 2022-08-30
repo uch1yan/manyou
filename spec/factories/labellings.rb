d104551441bc01de0993309ebaabdbf2b9d46a26
manami young <manayou1125@outlook.jp>
2022-08-30 14:29:44 +0900

    Inplement a test for label

--- spec/factories/labellings.rb
+++ spec/factories/labellings.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :labelling do
-    task { nil }
-    label { nil }
+    task
+    label
   end
 end
--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :label do
-    name { "MyString" }
+    name { "label_name" }
+  end
+
+  factory :label2, class: Label do
+    name { "label_name2" }
   end
 end
